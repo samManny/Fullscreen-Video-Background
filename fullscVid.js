192d38d075c38cea2f1c398a845ac346b6c5fc80
Emmanuel Samuel Ochwaya <58857140+samManny@users.noreply.github.com>
2021-07-22 07:23:30 +0300

    Add files via upload

---
+++ fullscVid.js
@@ -0,0 +1,20 @@
+﻿// Get the video
+
+var video = document.getElementById("myVideo");
+
+// Get your button
+
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, then change the text to either play/pause 
+
+function myFunction() {
+
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play"
+    }
+}
